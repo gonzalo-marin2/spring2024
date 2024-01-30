da7430c21c4e6379119f235ea3bbdfcc3ada3f03
tarde <tarde@A13E34>
2024-01-30 20:18:10 +0100

    proyecto 7 terminado

---
+++ 07_crud_productos_dao/src/main/java/controller/ProductosController.java
@@ -0,0 +1,49 @@
+package controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import model.Producto;
+import service.interfaces.ProductosService;
+
+@Controller
+public class ProductosController {
+	@Autowired
+	ProductosService productosService;
+	
+	@PostMapping(value="agregar")
+	public String agregar(@ModelAttribute Producto producto) {
+		productosService.agregarProducto(producto);
+		return "menu";
+	}
+	
+	@GetMapping(value="buscar")
+	public String buscar(@RequestParam("categoria") String categoria, Model model) {
+		model.addAttribute("productos", productosService.buscarPorCategoria(categoria));
+		return "productos";
+	}
+	
+	@GetMapping(value="eliminar")
+	public String eliminar(@RequestParam("nombre") String nombre, Model model) {
+		Producto prod=productosService.eliminarProducto(nombre);
+		if(prod!=null) {
+			model.addAttribute("mensajeEliminar", "se ha eliminado el producto "+nombre+" de la base de datos");
+		}else {
+			model.addAttribute("mensajeEliminar", "el producto "+nombre+" no existe");
+		}
+		return "menu";	
+	}
+	
+	@GetMapping(value="modificar")
+	public String actualizar(@RequestParam("nombre") String nombre, @RequestParam("nuevoPrecio") double nuevoPrecio, Model model) {
+		productosService.modificarPrecio(nombre, nuevoPrecio);
+		return "menu";
+	}
+}
---
+++ 07_crud_productos_dao/src/main/java/service/implementations/ProductosServiceImpl.java
@@ -0,0 +1,58 @@
+package service.implementations;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import dao.interfaces.ProductosDao;
+import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
+import model.Producto;
+import service.interfaces.ProductosService;
+
+@Service
+public class ProductosServiceImpl implements ProductosService {
+
+	@Autowired
+	ProductosDao productosDao;
+	
+	@Transactional
+	@Override
+	public boolean agregarProducto(Producto producto) {
+		if(productosDao.findByNombre(producto.getNombre())!=null) {
+			return false;
+		}
+		productosDao.save(producto);
+		return true;	
+	}
+
+	@Override
+	public List<Producto> buscarPorCategoria(String categoria) {
+		return productosDao.findByCategoria(categoria);
+	}
+
+	@Transactional
+	@Override
+	public void modificarPrecio(String nombre, double nuevoPrecio) {
+		Producto prod=productosDao.findByNombre(nombre);
+		if(prod!=null) {
+			prod.setPrecio(nuevoPrecio);
+			productosDao.update(prod);
+		}
+	}
+
+	@Transactional
+	@Override
+	public Producto eliminarProducto(String nombre) {
+		Producto prod=productosDao.findByNombre(nombre);
+		if(prod==null) {
+			return null;
+		}
+		//productosDao.delete(prod);
+		productosDao.deleteByNombre(nombre);
+		return prod;
+	}
+	
+}
