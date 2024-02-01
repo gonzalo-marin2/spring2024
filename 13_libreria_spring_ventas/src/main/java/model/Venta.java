8a3befa666faba0c360500e173c395395f498ee6
tarde <tarde@A13E34>
2024-02-01 20:48:53 +0100

    javabean Venta

--- 13_libreria_spring_ventas/src/main/java/model/Venta.java
+++ 13_libreria_spring_ventas/src/main/java/model/Venta.java
@@ -1,5 +1,26 @@
 package model;
 
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Entity
+@Table(name="ventas")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Venta {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int idVenta;
+	private int idCliente;
+	private int idLibro;
+	private Date fecha;
 
 }
